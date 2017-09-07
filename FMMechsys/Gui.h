ed6c2d55ab90d440e069c74dffe899f5beb7fbdd
Sergio Galindo Torres <galindotorres@hotmail.com>
2017-09-07 18:01:14 +1000

    macro file now can be selected

--- FMMechsys/Gui.h
+++ FMMechsys/Gui.h
@@ -36,8 +36,11 @@ namespace FMMechsys {
 		}
 
 	private:
-		String ^ filename;     //h5 file simple 
-		String ^ filenameFP;
+		String ^ filename;     //h5 file simple
+		String ^ macroname;    //name of the macro file
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::TextBox^  textBox1;
+			 String ^ filenameFP;
 
 	protected:
 		/// <summary>
@@ -75,6 +78,8 @@ namespace FMMechsys {
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
 			// 
 			// label1
@@ -82,10 +87,10 @@ namespace FMMechsys {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(6, 26);
-			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->label1->Location = System::Drawing::Point(16, 62);
+			this->label1->Margin = System::Windows::Forms::Padding(5, 0, 5, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(144, 17);
+			this->label1->Size = System::Drawing::Size(343, 39);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Open h5 Mechsys file";
 			this->label1->Click += gcnew System::EventHandler(this, &Gui::label1_Click);
@@ -95,20 +100,20 @@ namespace FMMechsys {
 			this->label2->AutoSize = true;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(31, 105);
-			this->label2->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->label2->Location = System::Drawing::Point(128, 454);
+			this->label2->Margin = System::Windows::Forms::Padding(5, 0, 5, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(199, 17);
+			this->label2->Size = System::Drawing::Size(474, 39);
 			this->label2->TabIndex = 1;
 			this->label2->Text = L"Created by Sergio Torres PhD";
 			this->label2->Click += gcnew System::EventHandler(this, &Gui::label2_Click);
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(34, 61);
-			this->button1->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
+			this->button1->Location = System::Drawing::Point(190, 311);
+			this->button1->Margin = System::Windows::Forms::Padding(5, 5, 5, 5);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(138, 35);
+			this->button1->Size = System::Drawing::Size(368, 83);
 			this->button1->TabIndex = 3;
 			this->button1->Text = L"Generate";
 			this->button1->UseVisualStyleBackColor = true;
@@ -116,24 +121,48 @@ namespace FMMechsys {
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(167, 23);
+			this->button2->Location = System::Drawing::Point(562, 46);
+			this->button2->Margin = System::Windows::Forms::Padding(8, 7, 8, 7);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->Size = System::Drawing::Size(200, 55);
 			this->button2->TabIndex = 4;
 			this->button2->Text = L"...";
 			this->button2->UseVisualStyleBackColor = true;
 			this->button2->Click += gcnew System::EventHandler(this, &Gui::button2_Click);
 			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(16, 195);
+			this->label3->Margin = System::Windows::Forms::Padding(5, 0, 5, 0);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(444, 39);
+			this->label3->TabIndex = 5;
+			this->label3->Text = L"Name of Fracman macro file";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(562, 195);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(205, 38);
+			this->textBox1->TabIndex = 6;
+			this->textBox1->Text = L"macro";
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &Gui::textBox1_TextChanged);
+			// 
 			// Gui
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(16, 31);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(260, 131);
+			this->ClientSize = System::Drawing::Size(779, 540);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
-			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
+			this->Margin = System::Windows::Forms::Padding(5, 5, 5, 5);
 			this->Name = L"Gui";
 			this->Text = L"Fracman+Mechsys";
 			this->ResumeLayout(false);
@@ -152,7 +181,7 @@ namespace FMMechsys {
 		std::string fname = marshal_as<std::string>(ftemp);
 
 		//Open the file and dataset containing the fractures
-		hid_t file_id;		
+		hid_t file_id;
 		file_id = H5Fopen(fname.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
 		if (file_id < 0)
 		{
@@ -161,9 +190,9 @@ namespace FMMechsys {
 			splash->ShowDialog();
 			return;
 		}
-		
+
 		hid_t dataset;
-		dataset    = H5Dopen(file_id, "/Verts", H5P_DEFAULT);
+		dataset = H5Dopen(file_id, "/Verts", H5P_DEFAULT);
 		if (dataset < 0)
 		{
 			Splash ^ splash = gcnew Splash;
@@ -172,24 +201,24 @@ namespace FMMechsys {
 			return;
 		}
 
-		hid_t dspace   = H5Dget_space(dataset);
+		hid_t dspace = H5Dget_space(dataset);
 		hssize_t hsize = H5Sget_simple_extent_ndims(dspace);
 
 		size_t ndims = hsize;
 		hsize_t * dims;
-		dims = new hsize_t[ndims]; 
+		dims = new hsize_t[ndims];
 		H5Sget_simple_extent_dims(dspace, dims, NULL);
 
 		size_t nfrac = dims[0] / 15;
 
-		
+
 
 		//Gettings number of fractures		
 		String ^ Nfractures = "Number of fractures ";
 		Nfractures = Nfractures + System::Convert::ToString(nfrac);
 
 
-		
+
 		Splash ^ splash = gcnew Splash;
 		splash->ModifyL1(Nfractures);
 		splash->ShowDialog();
@@ -221,10 +250,11 @@ namespace FMMechsys {
 		file.open("test.txt");
 		std::streambuf* sbuf = std::cout.rdbuf();
 		std::cout.rdbuf(file.rdbuf());*/
-
-		std::ofstream myfile("macro.fmf");
+		ftemp = macroname;
+		fname = marshal_as<std::string>(ftemp);
+		std::ofstream myfile(fname.c_str());
 		// Writing Fractures to Fracman fracture file
-		for (size_t nf = 0; nf < nfrac; nf = nf+2)
+		for (size_t nf = 0; nf < nfrac; nf = nf + 2)
 		{
 			double x = Verts[5 * nf + 4](0);
 			double y = Verts[5 * nf + 4](1);
@@ -240,13 +270,13 @@ namespace FMMechsys {
 
 			if (d1xd2(2) > 0.0) d1xd2 = -d1xd2;
 
-			double phi   = acos(d1xd2.dot(nx) / d1xd2.norm());
-			double theta = M_PI/2.0;
-			double lf = d3.norm()/2.0;
+			double phi = acos(d1xd2.dot(nx) / d1xd2.norm());
+			double theta = M_PI / 2.0;
+			double lf = d3.norm() / 2.0;
 
 			myfile << " BEGIN SingleFracture \n";
 			myfile << "	Name = \"Single Fracture " << nf + 1 << "\" \n";
-			myfile << "	Set = \"SF" << nf/2 + 1 << "\" \n";
+			myfile << "	Set = \"SF" << nf / 2 + 1 << "\" \n";
 			myfile << "	Center = " << x << ", " << y << ", " << z << "\n";
 			myfile << "	PoleTrPl trend = " << theta * 180 / M_PI << " plunge = " << phi * 180 / M_PI << "\n";
 			myfile << "	Radius = " << lf << "\n";
@@ -272,15 +302,18 @@ namespace FMMechsys {
 
 
 	}
-private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
-	OpenFileDialog ^ openFileDialog1 = gcnew OpenFileDialog();
-	openFileDialog1->Filter = "h5 Files|*.h5";
-	openFileDialog1->Title = "Select an h5 File";
-	if (openFileDialog1->ShowDialog() == System::Windows::Forms::DialogResult::OK)
-	{
-		filename   = System::IO::Path::GetFileName(openFileDialog1->FileName);
-		filenameFP = System::IO::Path::GetFullPath(openFileDialog1->FileName);
+	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+		OpenFileDialog ^ openFileDialog1 = gcnew OpenFileDialog();
+		openFileDialog1->Filter = "h5 Files|*.h5";
+		openFileDialog1->Title = "Select an h5 File";
+		if (openFileDialog1->ShowDialog() == System::Windows::Forms::DialogResult::OK)
+		{
+			filename = System::IO::Path::GetFileName(openFileDialog1->FileName);
+			filenameFP = System::IO::Path::GetFullPath(openFileDialog1->FileName);
+		}
 	}
-}
-};
+	private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+		macroname = textBox1->Text;
+	}
+	};
 }
